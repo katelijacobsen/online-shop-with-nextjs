9a1b825ed219da2ee458020a6d8be2f8d1267a3f
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-11 15:40:14 +0100

    rmv nav-elements

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,19 +23,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/">Home</a>
-            </li>
-          </ul>
-        </nav>
         {children}
       </body>
     </html>
