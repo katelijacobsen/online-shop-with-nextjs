a9e0650a0b70b94e971a2dc8a94607a024ab4b8f
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-14 00:27:06 +0100

    add readOnly

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,7 +1,7 @@
 const CategoryItem = ({ category, onSelect, checked }) => {
   return (
     <li onClick={() => onSelect(category)} className="hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white hover:rounded-lg py-1 px-2">
-      <input checked={checked} type="checkbox"/>
+      <input readOnly checked={checked} type="checkbox"/>
       <button className="inline-flex items-center text-md">
         {category.icon && <span className="m-2">{category.icon}</span>}
         {category.name}
