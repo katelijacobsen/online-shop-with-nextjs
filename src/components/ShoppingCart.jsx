033aa7c7f1a6e8a70b0447985440b521a4c2e605
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-21 08:05:28 +0100

    changes

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -46,19 +46,19 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
                     >
                       <IoTrashOutline className="mr-1" /> Remove
                     </button>
-                    <div class="max-w-xs mx-auto">
+                    <div className="max-w-xs mx-auto">
                       <label
-                        for="quantity-input"
-                        class="block text-sm font-medium text-gray-900 dark:text-white"
+                        htmlFor="quantity-input"
+                        className="block text-sm font-medium text-gray-900 dark:text-white"
                       >
                         Choose quantity:
                       </label>
-                      <div class="relative flex items-center">
+                      <div className="relative flex items-center">
                         <button
                           type="button"
                           id="decrement-button"
                           data-input-counter-decrement="quantity-input"
-                          class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-2 h-8 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                          className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-2 h-8 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
                         >
                           <FaMinus />
                         </button>
@@ -67,7 +67,7 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
                           id="quantity-input"
                           data-input-counter
                           aria-describedby="helper-text-explanation"
-                          class="bg-gray-50 border-x-0 border-gray-300 h-8 w-8 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border-x-0 border-gray-300 h-8 w-8 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="999"
                           required
                         />
@@ -75,7 +75,7 @@ const ShoppingCart = ({ cart, removeFromCart }) => {
                           type="button"
                           id="increment-button"
                           data-input-counter-increment="quantity-input"
-                          class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-2 h-8 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
+                          clasName="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-2 h-8 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
                         >
                           <FaPlus />
                         </button>
