cc1b870e0844594a5e41e76172ab50bb562f3730
katelijacobsen <katjamaehleke98@gmail.com>
2024-11-22 17:24:23 +0100

    I'm suffering from divitis

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,7 +55,7 @@ export default function RootLayout({ children }) {
           variants={pageTransition}
         >
           {children}
-        </motion.div>
+        </motion.div> 
         <Footer />
         {visible && (
           <div className="fixed inset-0 z-50">
